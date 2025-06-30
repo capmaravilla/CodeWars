f1e8a51009af3965767aaa813ed51f8949fa220b
David Moriez <61180214+capmaravilla@users.noreply.github.com>
2025-06-30 12:56:55 +0200

    org II

--- 01-10.js
+++ 01-10.js
@@ -1,31 +1,20 @@
 // * -------------------------------------------------------------------
 // * Next bigger number with the same digits - 4kyu
+// https://www.codewars.com/kata/55983863da40caa2c900004e
 // * -------------------------------------------------------------------
-// ? Mirar que hacemos en cada momento
 function nextBigger(n) {
   let cache = n.toString().split('').map(Number)
   for (let i = cache.length - 2; i >= 0; i--) {
-    let rang = cache.slice(i)
-    let rangMax = cache.slice(i).sort((a, b) => b - a)
-
-    console.log(rang)
-    console.log(rangMax)
-    let tail = [...rang].sort()
-    console.log(tail)
-
-    let n = tail.splice(1,1)
-    console.log(tail)
-    console.log(n)
-
+    let rang = [...cache].slice(i)
+    let rangMax = [...cache].slice(i).sort((a, b) => b - a)
     if (rangMax > rang) {
-
-      console.log(cache.slice(0,i))
-      console.log(rang)
-      console.log(tail)
-      console.log(cache.slice(0, i).join('') + ' '+ n + ' '+ tail.join(''))
-
-      return Number(cache.slice(0, i).join('') + n + tail.join(''))
-
+      let sortTail = [...rang].sort()
+      let unique = new Set(sortTail)
+      unique = [...unique]
+      let idx = unique.indexOf(rang[0])
+      let n = unique[idx+1]
+      sortTail.splice(sortTail.indexOf(n),1)
+      return Number(cache.slice(0, i).join('') + n + sortTail.join(''))
     }
   }
   return -1
