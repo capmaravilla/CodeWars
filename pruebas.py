9e06a009170de50d62068d92166f06185477d41c
David Moriez <capmaravilla@gmail.com>
2025-07-05 13:28:32 +0200

    score 1064

--- pruebas.py
+++ pruebas.py
@@ -1,13 +1,1 @@
-def data_reverse(data):
-    array = [x for x in data]
-    final = []
-    for _ in range(int(len(array) / 8)):
-        a = [array.pop(0) for _ in range(8)]
-        final.append(a)
-
-    return final
-
-
-bits = [1, 0, 1, 0, 1, 0, 1, 0, 2, 0, 2, 0, 2, 0, 2, 0, 3, 0, 3, 0, 3, 0, 3, 0]
-
-print(data_reverse(bits))
+º
