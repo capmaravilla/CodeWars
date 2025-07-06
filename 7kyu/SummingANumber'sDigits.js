972ad8bd523903cc028e257169f7812cee5f96b6
David Moriez <capmaravilla@gmail.com>
2025-07-06 09:55:44 +0200

    1098 faciles

---
+++ 7kyu/SummingANumber'sDigits.js
@@ -0,0 +1,21 @@
+// * -------------------------------------------------------------------
+// * Summing a number's digits - 7kyu
+// https://www.codewars.com/kata/52f3149496de55aded000410/train/javascript
+// * -------------------------------------------------------------------
+
+function sumDigits(number) {
+  return Math.abs(number)
+    .toString()
+    .split("")
+    .reduce((acc, el) => +acc + +el, 0);
+}
+
+function sumDigits1(number) {
+  let sum = 0;
+  for (const digit of Math.abs(number).toString()) {
+    sum += +digit;
+  }
+  return sum;
+}
+
+console.log(sumDigits(-99));
